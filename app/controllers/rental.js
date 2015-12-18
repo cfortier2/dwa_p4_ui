361b3f5b2cc1085426536149706d4039b712902b
Chris Fortier <cfortier2@gmail.com>
2015-12-18 17:52:12 -0500

    sort of working updates

--- app/controllers/rental.js
+++ app/controllers/rental.js
@@ -2,13 +2,37 @@ import Ember from 'ember';
 
 export default Ember.Controller.extend({
   isDisabled: true,
+  model_id: '',
   actions: {
-    updateListing() {
+    enableEdit() {
       this.set('isDisabled', false);
     },
-    saveListing() {
+    updateListing(id) {
       this.set('isDisabled', true);
-      // implement update
+      var emailAddress = this.get('emailAddress');
+      var title = this.get('title');
+      var owner = this.get('owner');
+      var city = this.get('city');
+      var type = this.get('type');
+      var image = this.get('image');
+      var price = this.get('price');
+      var available_month = this.get('available_month');
+      var summary = this.get('summary');
+
+      console.log(id);
+      this.store.findRecord('rental', id).then(function(rental) {
+        rental.set('emailAddress', emailAddress);
+        rental.set('title', title);
+        rental.set('owner', owner);
+        rental.set('city', city);
+        rental.set('type', type);
+        rental.set('image', image);
+        rental.set('price', price);
+        rental.set('available_month', available_month);
+        rental.set('summary', summary);
+        rental.save();
+      });
+
     },
     deleteListing() {
       this.set('isDisabled', true);
