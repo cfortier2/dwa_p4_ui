9a7dabab8d60ab5874ce1a9fb9c9306a25c9ff50
Chris Fortier <cfortier2@gmail.com>
2015-12-20 22:40:16 -0500

    everything actually works

--- app/models/rental.js
+++ app/models/rental.js
@@ -5,10 +5,10 @@ export default DS.Model.extend({
   owner: DS.attr(),
   city: DS.attr(),
   type: DS.attr(),
-  image: DS.attr(),
   price: DS.attr(),
   available_month: DS.attr(),
   summary: DS.attr(),
   emailAddress: DS.attr(),
   uniqid: DS.attr(),
+  images: DS.attr(),
 });
